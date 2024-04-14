0d6c9394b5471d554321a8fde641faa1a263e341
oscardenas <oscardenas17@gmail.com>
2024-04-14 15:31:55 -0500

    Eliminar paciente

--- src/components/PatientDetail.tsx
+++ src/components/PatientDetail.tsx
@@ -1,3 +1,4 @@
+import { usePatientStore } from "../store";
 import type { Patient } from "../types";
 import PatientDetailItem from "./PatientDetailItem";
 
@@ -5,6 +6,7 @@ type PatientDetailsProps = {
   patient: Patient;
 };
 const PatientDetail = ({ patient }: PatientDetailsProps) => {
+  const deletePatient = usePatientStore((state) => state.deletePatient);
   return (
     <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl ">
       <PatientDetailItem label={"ID"} data={patient.id} />
@@ -13,6 +15,22 @@ const PatientDetail = ({ patient }: PatientDetailsProps) => {
       <PatientDetailItem label={"Email"} data={patient.email} />
       <PatientDetailItem label={"Fecha alta"} data={patient.date.toString()} />
       <PatientDetailItem label={"Sintomas"} data={patient.symptoms} />
+
+      <div className="flex justify-between  mt-10">
+        <button
+          type="button"
+          className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+        >
+          Editar
+        </button>
+        <button
+          type="button"
+          className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+          onClick={() => deletePatient(patient.id)}
+        >
+          Eliminar
+        </button>
+      </div>
     </div>
   );
 };
--- src/store.ts
+++ src/store.ts
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 type PatientState = {
     patients:Patient[]
     addPatient:(data:DraftPatient) => void
+    deletePatient: (id:Patient['id']) => void
 }
 
 
@@ -18,6 +19,12 @@ export const usePatientStore = create<PatientState>( (set)=>({
         set( (state) =>({
         patients: [...state.patients, newPatient]
     })) 
-   }
+   },
+
+   deletePatient:(id)=>{ set((state)=>({
+    patients: state.patients.filter(patient => patient.id !== id)
+   })) }
+
+
 
 }))
