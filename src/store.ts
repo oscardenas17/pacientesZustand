99efd9b3818515f4260c895b0da2644953d03123
oscardenas <oscardenas17@gmail.com>
2024-04-14 18:47:13 -0500

    Persisitiendo la informacion

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { DraftPatient, Patient } from "./types";
 import { v4 as uuidv4 } from "uuid";
-import { devtools } from "zustand/middleware";
+import {  devtools, persist } from "zustand/middleware";
 
 type PatientState = {
   patients: Patient[];
@@ -15,54 +15,60 @@ type PatientState = {
 const createPatient = (patient: DraftPatient): Patient => {
   return { ...patient, id: uuidv4() };
 };
-export const usePatientStore = create<PatientState>()<
-  [["zustand/devtools", never]]
->(
-  devtools((set) => ({
-    patients: [],
-    activeId: "",
-    addPatient: (data) => {
-      const newPatient = createPatient(data);
-      set(
-        (state) => ({
-          patients: [...state.patients, newPatient],
-        }),
-        false,
-        { type: "addPatient" }
-      );
-    },
-    deletePatient: (id) => {
-      set(
-        (state) => ({
-          patients: state.patients.filter((patient) => patient.id !== id),
-        }),
-        false,
-        { type: "deletePatient" }
-      );
-    },
-    getPatientById(id) {
-      set(
-        () => ({
-          activeId: id,
-        }),
-        false,
-        { type: "getPatientById" }
-      );
-    },
+export const usePatientStore = create<PatientState>()(
+  devtools(
+    persist(
+      (set) => ({
+        patients: [],
+        activeId: "",
+        addPatient: (data) => {
+          const newPatient = createPatient(data);
+          set(
+            (state) => ({
+              patients: [...state.patients, newPatient],
+            }),
+            false,
+            { type: "addPatient" }
+          );
+        },
+        deletePatient: (id) => {
+          set(
+            (state) => ({
+              patients: state.patients.filter((patient) => patient.id !== id),
+            }),
+            false,
+            { type: "deletePatient" }
+          );
+        },
+        getPatientById(id) {
+          set(
+            () => ({
+              activeId: id,
+            }),
+            false,
+            { type: "getPatientById" }
+          );
+        },
 
-    updatePatient(data) {
-      set(
-        (state) => ({
-          patients: state.patients.map((patient) =>
-            patient.id === state.activeId
-              ? { id: state.activeId, ...data }
-              : patient
-          ),
-          activeId: "",
-        }),
-        false,
-        { type: "updatePatient" }
-      );
-    },
-  }))
+        updatePatient(data) {
+          set(
+            (state) => ({
+              patients: state.patients.map((patient) =>
+                patient.id === state.activeId
+                  ? { id: state.activeId, ...data }
+                  : patient
+              ),
+              activeId: "",
+            }),
+            false,
+            { type: "updatePatient" }
+          );
+        },
+      }),
+      {
+        name: "patient-storage",
+        // storage: createJSONStorage(() => localStorage),
+      }
+    )
+  )
 );
